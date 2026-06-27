98a5706763bd1856add3c9551d9c71f92e7a5410
thando-dlamini-dev <thandodlaminidev@gmail.com>
2026-06-27 22:06:39 +0200

    add gitignore

---
+++ backend/src/server.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+import cors from "cors";
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const PORT = process.env.PORT || 5000
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+    console.log(`Started at ${Date()}`);
+});
