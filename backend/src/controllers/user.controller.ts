a5d1df5fe7b683b5ceae266753ca5079d27f046d
thando-dlamini-dev <thandodlaminidev@gmail.com>
2026-06-28 11:10:21 +0200

    update user controller

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -6,10 +6,15 @@ import {eq} from "drizzle-orm";
 export const getUserById = async (req: Request, res: Response) => {
     try {
         const { id } = req.params;
-        const user = await db.select().from(users).where(eq(users.id, id))
+        const userId = Number(id)//convert id to number
+        const user = await db.select().from(users).where(eq(users.id, userId))
 
+        //throw error if user is not found
         if(!user){
-            throw new Error("User not found");
+            return res.status(404).json({
+                success: false,
+                error: "User not found"
+            })
         }
 
         return res.status(200).json({
@@ -18,6 +23,29 @@ export const getUserById = async (req: Request, res: Response) => {
         });
     }
     catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            error: "Error while trying to retrieve user"
+        })
+    }
+}
 
+export const getAllUsers = async (req: Request, res: Response) => {
+    try {
+        const user = await db.select().from(users)
+        if(!users){
+            return res.status(404).json({
+                success: false,
+                error: "Users not found"
+            })
+        }
+    }
+    catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            error: "Error while trying to retrieve all users"
+        })
     }
 }
