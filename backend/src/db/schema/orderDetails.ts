d034eaf4ea9ae79309872cab3e852b4a72285d4e
thando-dlamini-dev <thandodlaminidev@gmail.com>
2026-07-20 11:55:57 +0200

    add and update tables

---
+++ backend/src/db/schema/orderDetails.ts
@@ -0,0 +1,27 @@
+import {integer, pgTable, text, uuid} from "drizzle-orm/pg-core";
+import orders from "./orders";
+import {timestamps} from "../columns.helpers";
+
+const orderDetails = pgTable("order_details", {
+    id: uuid("id").defaultRandom().notNull().unique(),
+    orderID: uuid().notNull().references(() => orders.orderId, {onDelete: "cascade"}),
+
+    //Product Dimensions
+    widthMm: integer("width_mm"),
+    heightMm: integer("height_mm"),
+    depthMM: integer("depth_mm"),
+
+    //Top, Side & Front view images of the product
+    referenceImages: text("reference_images").array().notNull(),
+
+    //Logos, Labels and product info images
+    artworkFiles: text("artwork_files").array(),
+
+    // Creative preferences
+    backgroundStyle: text("background_style").default("transparent"), // white, dark, transparent
+    materialNotes: text("material_notes"),
+
+    ...timestamps
+})
+
+export default orderDetails;
